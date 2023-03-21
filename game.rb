c880bf4975898aa8c1bcc2e2ab4f7174d61be481
NewCreation4 <105675178+NewCreation4@users.noreply.github.com>
2023-03-21 11:33:36 -0300

    Made map 2d.

--- game.rb
+++ game.rb
@@ -1,54 +1,83 @@
-wall="!"
-win="$"
-tile="#"
-player="&"
+P="\e[34m\e[1m&\e[0m"
+T="\e[90m#\e[0m"
+W="\e[94m\e[2m!\e[0m"
+G="\e[32m$\e[0m"
 
-map = [wall,player,tile,tile,tile,tile,win,wall]
-mlen=map.length()
+map=[
+  [W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W],
+  [W,P,W,T,T,T,W,T,W,T,W,T,T,T,T,T,T,T,W],
+  [W,T,T,T,W,T,T,T,T,T,T,T,T,W,W,W,W,T,W],
+  [W,T,W,T,T,T,T,T,W,W,W,W,W,W,T,T,T,T,W],
+  [W,W,W,W,W,W,W,W,W,W,T,T,W,T,T,T,W,W,W],
+  [W,G,T,T,T,T,T,T,T,T,T,T,T,T,W,T,T,T,W],
+  [W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W]
+]
+mapMessages={
+  "onWinMessage"=>"u win",
+}
+
 px=1
-puts "Welcome to some random game."
-puts "
-Actions:
->   Move Forward
-<   Move Backwards
-"
+py=1
+
+define_singleton_method("showmap") {
+  map.each do |line|
+    line.each do |tile|
+      print "#{tile} "
+    end
+    print "\n"
+  end
+}
+define_singleton_method("win") {
+  puts mapMessages["onWinMessage"]
+  exit
+}
+showmap
+
+puts """
+v is down
+^ is up
+> is frontwards
+< is backwards
+"""
 
 while true
-  puts map[0]+map[1]+map[2]+map[3]+map[4]+map[5]+map[6]+map[7]
-  a=gets.chomp()
-  if a==">"
-    for i in 0..7
-      if map[i+1]=="#"
-        if px==i
-          map[i+1]="&"
-          map[i]="#"
-          px=px+1
-          break
-        end
-      elsif map[i+1]=="$"
-        if px==i
-          puts "You Won!"
-          exit()
-          break
-        end
-      end
+  print "Move: "
+  move=gets.chomp
+  system("clear")
+  
+  case move
+  when ">"
+    if map[py][px+1]==T 
+      map[py][px]=T
+      map[py][px+1]=P
+      px+=1
+    elsif map[py][px+1]==G
+      win
     end
-  elsif a=="<"
-    for i in 0..7
-      if map[i-1]=="#"
-        if px==i
-          map[i-1]="&"
-          map[i]="#"
-          px=px-1
-          break
-        end
-      elsif map[i-1]=="$"
-        if px==i
-          puts "You Won!"
-          exit()
-          break
-        end
-      end
+  when "<"
+    if map[py][px-1]==T 
+      map[py][px]=T
+      map[py][px-1]=P
+      px-=1
+    elsif map[py][px-1]==G
+      win
+    end
+  when "^"
+    if map[py-1][px]==T 
+      map[py][px]=T
+      map[py-1][px]=P
+      py-=1
+    elsif map[py-1][px]==G
+      win
+    end
+  when "v"
+    if map[py+1][px]==T 
+      map[py][px]=T
+      map[py+1][px]=P
+      py+=1
+    elsif map[py+1][px]==G
+      win
     end
   end
+  showmap
 end
